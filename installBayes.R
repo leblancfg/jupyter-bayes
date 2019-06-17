62a74cfdd6d76dd341caf510a4ed0d2387b02120
François Leblanc <leblancfg@gmail.com>
2019-06-17 09:06:34 -0400

    Add runstan, parallel, and compute.es

--- installBayes.R
+++ installBayes.R
@@ -4,10 +4,14 @@
 # biocLite("Rgraphviz")
 
 install.packages("rjags", repos="https://cloud.r-project.org/", dependencies=NA, clean=TRUE) 
+install.packages("runjags", repos="https://cloud.r-project.org/", dependencies=NA, clean=TRUE) 
 install.packages("HydeNet", repos="https://cloud.r-project.org/", dependencies=NA, clean=TRUE) 
 
 install.packages("gRain", repos="https://cloud.r-project.org/", dependencies=TRUE, clean=TRUE)
 install.packages("gRim", repos="https://cloud.r-project.org/", dependencies=TRUE, clean=TRUE)
 
 install.packages("bnlearn", repos="https://cloud.r-project.org/", dependencies=TRUE, clean=TRUE)
+
 install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies = TRUE)
+install.packages("parallel", repos="https://cloud.r-project.org/", dependencies=NA, clean=TRUE)
+install.packages("compute.es", repos="https://cloud.r-project.org/", dependencies=NA, clean=TRUE) 
